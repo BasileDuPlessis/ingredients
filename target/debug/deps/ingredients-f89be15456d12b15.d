181682e13d88dae40d8a7846e6839d2b827bf477
Basile du Plessis <basile.duplessis@gmail.com>
2025-09-05 11:31:28 +0200

    Initial commit

---
+++ target/debug/deps/ingredients-f89be15456d12b15.d
@@ -0,0 +1,5 @@
+/Users/basile.du.plessis/Documents/ingredients/target/debug/deps/libingredients-f89be15456d12b15.rmeta: src/main.rs
+
+/Users/basile.du.plessis/Documents/ingredients/target/debug/deps/ingredients-f89be15456d12b15.d: src/main.rs
+
+src/main.rs:
