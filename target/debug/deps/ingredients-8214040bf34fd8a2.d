4f03d6b4c627ee2b2b595f4bf3c5a77ecebdce2f
Basile du Plessis <basile.duplessis@gmail.com>
2025-09-05 15:23:34 +0200

    Remove build artifacts from target/ directory

--- target/debug/deps/ingredients-8214040bf34fd8a2.d
+++
@@ -1,5 +0,0 @@
-/Users/basile.du.plessis/Documents/ingredients/target/debug/deps/libingredients-8214040bf34fd8a2.rmeta: src/main.rs
-
-/Users/basile.du.plessis/Documents/ingredients/target/debug/deps/ingredients-8214040bf34fd8a2.d: src/main.rs
-
-src/main.rs:
